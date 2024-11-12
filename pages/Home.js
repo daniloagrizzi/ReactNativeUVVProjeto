0cbc4aa8a94891d220b04d322dc6c1346a30ad08
DANILO AGRIZZI <daniloagrizzi@hotmail.com>
2024-11-12 12:06:14 -0300

    Adicionadas verificações de login

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Modal, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
-
-
 
 export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false); 
+  const [errorMessage, setErrorMessage] = useState(''); 
   const navigation = useNavigation();
 
   const togglePasswordVisibility = () => {
@@ -21,46 +21,69 @@ export default function Home() {
       const { email: storedEmail, password: storedPassword, name } = JSON.parse(userData);
       if (email === storedEmail && password === storedPassword) {
         navigation.navigate('Welcome', { name });
-      } else {
-        Alert.alert('Erro', 'Email ou senha incorretos.');
+      } else if (email !== storedEmail) {
+        setErrorMessage('Email incorreto.');
+        setModalVisible(true); 
+      } else if (password !== storedPassword) {
+        setErrorMessage('Senha incorreta.');
+        setModalVisible(true); 
       }
     } else {
-      Alert.alert('Erro', 'Nenhum usuário cadastrado.');
+      setErrorMessage('Nenhum usuário cadastrado.');
+      setModalVisible(true); 
     }
   };
 
   return (
-
-      <View style={styles.container}>
-        <Text style={styles.title}>Bem-vindo!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <View style={styles.passwordContainer}>
         <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
+          style={styles.inputSenha}
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!passwordVisible}
         />
-        <View style={styles.passwordContainer}>
-          <TextInput
-            style={styles.inputSenha}
-            placeholder="Senha"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry={!passwordVisible}
-          />
-          <TouchableOpacity onPress={togglePasswordVisibility} style={styles.eyeIcon}>
-            <Text>{passwordVisible ? '👁️' : '🙈'}</Text>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Entrar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Cadastro')} style={styles.linkButton}>
-          <Text style={styles.linkText}>Crie seu cadastro</Text>
+        <TouchableOpacity onPress={togglePasswordVisibility} style={styles.eyeIcon}>
+          <Text>{passwordVisible ? '👁️' : '🙈'}</Text>
         </TouchableOpacity>
       </View>
-   
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Cadastro')} style={styles.linkButton}>
+        <Text style={styles.linkText}>Crie seu cadastro</Text>
+      </TouchableOpacity>
+
+      {/* Modal de erro */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalMessage}>{errorMessage}</Text>
+            <Pressable
+              style={styles.modalButton}
+              onPress={() => setModalVisible(false)} // Fecha o modal
+            >
+              <Text style={styles.modalButtonText}>Fechar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 }
 
@@ -79,7 +102,7 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   input: {
-    width: '30%',
+    width: '80%', // Ajuste para melhorar o layout
     padding: 10,
     marginVertical: 10,
     borderWidth: 1,
@@ -88,7 +111,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   passwordContainer: {
-    width: '30%',
+    width: '80%',
     padding: 10,
     marginVertical: 10,
     borderWidth: 1,
@@ -101,7 +124,7 @@ const styles = StyleSheet.create({
     right: 10,
   },
   button: {
-    width: '10%',
+    width: '80%',
     padding: 15,
     backgroundColor: '#007BFF',
     borderRadius: 5,
@@ -118,6 +141,35 @@ const styles = StyleSheet.create({
   },
   linkText: {
     color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalMessage: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 20,
+  },
+  modalButton: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalButtonText: {
+    color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
   },
